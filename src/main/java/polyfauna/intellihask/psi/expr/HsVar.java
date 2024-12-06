e4873eb51159cde40a991134c40477ba1a33647e
l-Luna <izzeldeen03@gmail.com>
2024-12-06 23:29:52 +0000

    minor refactoring

--- src/main/java/polyfauna/intellihask/psi/expr/HsVar.java
+++ src/main/java/polyfauna/intellihask/psi/expr/HsVar.java
@@ -3,21 +3,16 @@ package polyfauna.intellihask.psi.expr;
 import com.intellij.lang.ASTNode;
 import com.intellij.model.Symbol;
 import com.intellij.model.psi.PsiSymbolReference;
-import com.intellij.openapi.roots.ProjectRootManager;
 import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiFile;
-import com.intellij.psi.PsiManager;
 import org.jetbrains.annotations.NotNull;
-import polyfauna.intellihask.psi.HsFile;
 import polyfauna.intellihask.psi.HsSymbolReference;
-import polyfauna.intellihask.psi.decl.HsNamedDecl;
-import polyfauna.intellihask.psi.symbol.PsiBindingSymbol;
+import polyfauna.intellihask.psi.symbol.BindingSymbol;
+import polyfauna.intellihask.psi.symbol.searcher.BindingSearcher;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class HsVar extends HsSymbolReference{
 	
@@ -42,24 +37,10 @@ public class HsVar extends HsSymbolReference{
 	}
 	
 	public @NotNull Collection<? extends Symbol> resolveReference(){
-		// resolve to a binding...
-		// which may come from any module in the project, plus from any module providers
-		// let's keep it simple here & generify after
-		// TODO: everything
-		List<PsiBindingSymbol> symbols = new ArrayList<>();
-		String name = name();
-		ProjectRootManager.getInstance(getProject()).getFileIndex().iterateContent(
-				file -> {
-					PsiFile pFile = PsiManager.getInstance(getProject()).findFile(file);
-					if(!(pFile instanceof HsFile hsf))
-						return true;
-					hsf.module().ifPresent(module -> {
-						if(module.decls().stream().anyMatch(x -> x instanceof HsNamedDecl hnd && hnd.names().contains(name)))
-							symbols.add(new PsiBindingSymbol(name, module));
-					});
-					return true;
-				},
-				file -> Objects.equals(file.getExtension(), "hs"));
-		return symbols;
+		return BindingSearcher.byName(getProject(), name()).collect(Collectors.toSet());
+	}
+	
+	public boolean resolvesTo(@NotNull Symbol target){
+		return target instanceof BindingSymbol bs && bs.name().equals(name()) && /* scope */ super.resolvesTo(target);
 	}
 }
---
+++ src/main/java/polyfauna/intellihask/psi/symbol/searcher/BindingSearcher.java
@@ -0,0 +1,42 @@
+package polyfauna.intellihask.psi.symbol.searcher;
+
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiManager;
+import polyfauna.intellihask.psi.HsFile;
+import polyfauna.intellihask.psi.decl.HsNamedDecl;
+import polyfauna.intellihask.psi.symbol.BindingSymbol;
+import polyfauna.intellihask.psi.symbol.PsiBindingSymbol;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+public final class BindingSearcher{
+
+	// ...from a read action
+	public static Stream<BindingSymbol> all(Project project){
+		List<VirtualFile> sources = new ArrayList<>();
+		ProjectRootManager.getInstance(project).getFileIndex().iterateContent(file -> {
+			sources.add(file);
+			return true;
+		}, file -> Objects.equals(file.getExtension(), "hs"));
+		PsiManager pm = PsiManager.getInstance(project);
+		return sources.stream()
+				.map(pm::findFile)
+				.filter(HsFile.class::isInstance)
+				.map(HsFile.class::cast)
+				.flatMap(hf -> hf.module().stream())
+				.flatMap(mod -> mod.decls().stream()
+						.filter(HsNamedDecl.class::isInstance)
+						.flatMap(decl -> ((HsNamedDecl)decl).names().stream())
+						.map(decl -> new PsiBindingSymbol(decl, mod)));
+	}
+	
+	public static Stream<BindingSymbol> byName(Project project, String name){
+		return all(project).filter(b -> b.name().equals(name));
+	}
+}
