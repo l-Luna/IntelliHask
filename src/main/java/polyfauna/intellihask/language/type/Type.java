67a8a4eb587dfcf00e55e91b01d7eab21c1bf0a9
l-Luna <izzeldeen03@gmail.com>
2024-12-19 11:51:35 +0000

    better type pretty printing

--- src/main/java/polyfauna/intellihask/language/type/Type.java
+++ src/main/java/polyfauna/intellihask/language/type/Type.java
@@ -13,15 +13,21 @@ public sealed interface Type{
 	record NamedTyCtor(String name) implements Type{}
 	
 	default String pretty(){
+		return pretty(false);
+	}
+	
+	default String pretty(boolean bracket){
+		String open = bracket ? "(" : "", close = (bracket ? ")" : "");
 		return switch(this){
 			case TyApp(Type head, List<Type> args) when head == Arrow.ARROW && args.size() == 2
-					-> args.get(0).pretty() + " -> " + args.get(1).pretty();
+					-> open + args.get(0).pretty(true) + " -> " + args.get(1).pretty() + close;
 			case TyApp(Type head, List<Type> args) when head == List_.LIST && args.size() == 1
 					-> "[" + args.getFirst().pretty() + "]";
 			case TyApp(Tuple(int count), List<Type> args) when args.size() == count
 					-> "(" + String.join(", ", args.stream().map(Type::pretty).toList()) + ")";
 			
-			case TyApp(Type head, List<Type> args) -> head.pretty() + " " + String.join(" ", args.stream().map(Type::pretty).toList());
+			case TyApp(Type head, List<Type> args)
+					-> open + head.pretty() + " " + String.join(" ", args.stream().map(type -> type.pretty(true)).toList()) + close;
 			case Arrow.ARROW -> "(->)";
 			case List_.LIST -> "[]";
 			case Tuple(int count) -> "(" + ",".repeat(Math.max(0, count - 1)) + ")";
